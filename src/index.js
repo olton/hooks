a54193a11d9795ad7d31b365f009c03a4eac5ba2
Serhii Pimenov <serhii@pimenov.com.ua>
2024-06-11 16:50:42 +0300

    add memoize

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import {useState} from "./state.js"
 import {useEvent} from "./event.js"
 import {useEffect, USE_EFFECT_EVENTS} from "./effect.js"
+import {useMemo} from "./memo.js";
 
-const version = "0.3.0"
-const build_time = "08.05.2024, 12:58:44"
+const version = "0.4.0"
+const build_time = "11.06.2024, 16:50:24"
 
 const info = () => {
     console.info(`%c Hooks %c v${version} %c ${build_time} `, "color: #ffffff; font-weight: bold; background: #5c2c05", "color: white; background: darkgreen", "color: white; background: #0080fe;")
@@ -13,5 +14,6 @@ export {
     useState,
     useEvent,
     useEffect, USE_EFFECT_EVENTS,
+    useMemo,
     info
 }
---
+++ src/memo.js
@@ -0,0 +1,17 @@
+const useMemo = fn => {
+    const cache = new Map()
+    return function(...args){
+        const key = "" + args.length + args.join("+")
+        if (cache.has(key)) {
+            return cache.get(key)
+        } else {
+            const result = fn(...args)
+            cache.set(key, result)
+            return result
+        }
+    }
+}
+
+export {
+    useMemo
+}
