b8e60c23385c72c6d29e31c5f24c77a8ead2a8d8
Serhii Pimenov <serhii@pimenov.com.ua>
2024-06-13 21:19:49 +0300

    upd readme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {useEffect, USE_EFFECT_EVENTS} from "./effect.js"
 import {useMemo} from "./memo.js";
 
 const version = "0.5.0"
-const build_time = "13.06.2024, 21:03:08"
+const build_time = "13.06.2024, 21:19:44"
 
 const info = () => {
     console.info(`%c Hooks %c v${version} %c ${build_time} `, "color: #ffffff; font-weight: bold; background: #5c2c05", "color: white; background: darkgreen", "color: white; background: #0080fe;")
