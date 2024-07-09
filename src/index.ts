1ebf3c9c4385f4e162be380221a116437305cc81
Serhii Pimenov <serhii@pimenov.com.ua>
2024-07-09 13:05:10 +0300

    fix types path in package.json

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import {useCurry} from "./curry/index.js";
 import {useCompose} from "./compose/index.js";
 import {useThrottle} from "./throttle/index.js";
 
-const version = "0.9.0"
-const build_time = "09.07.2024, 12:58:11"
+const version = "0.10.0"
+const build_time = "09.07.2024, 13:04:50"
 
 const info = () => {
     console.info(`%c Hooks %c v${version} %c ${build_time} `, "color: #ffffff; font-weight: bold; background: #5c2c05", "color: white; background: darkgreen", "color: white; background: #0080fe;")
