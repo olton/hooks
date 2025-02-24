2e448c7cd46cf0c2e81f38684861ee1e55443c9b
Serhii Pimenov <serhii@pimenov.com.ua>
2025-02-24 04:06:29 +0200

    fix useId

--- src/id/index.ts
+++ src/id/index.ts
@@ -1,11 +1,11 @@
 const id_store = new Map()
 let id_counter = 0
 
-const useId = (el: HTMLElement, prefix = 'id') => {
+const useId = (el: HTMLElement, prefix = 'x', divider = "-") => {
     if (id_store.has(el)) {
         return id_store.get(el)
     }
-    const id = `:${prefix}:${id_counter++}`
+    const id = `__id${divider}${prefix}${divider}${id_counter++}`
     id_store.set(el, id)
     return id    
 }
